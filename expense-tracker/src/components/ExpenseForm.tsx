f5edfb68f57bb81ba2264b2c1e5315b30da5b340
AiZm8 <zai@ualberta.ca>
2025-02-15 22:31:07 -0700

    fix tsx syntax

--- expense-tracker/src/components/ExpenseForm.tsx
+++ expense-tracker/src/components/ExpenseForm.tsx
@@ -1,6 +1,47 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import { useState, ChangeEvent, FormEvent } from 'react';
 
+// define type for inputs
+interface FormInputs {
+    description?: string;
+    amount?: number;
+}
 
-
-export default App
+function ExpenseForm() {
+    const [inputs, setInputs] = useState<FormInputs>({});
+  
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      // alert(inputs);
+      console.log(inputs);
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>Description:
+        <input 
+          type="text" 
+          name="description" 
+          value={inputs.description || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Amount:
+          <input 
+            type="number" 
+            name="amount" 
+            value={inputs.amount || ""} 
+            onChange={handleChange}
+          />
+          </label>
+          <input type="submit" />
+      </form>
+    )
+  }
+  
+export default ExpenseForm
