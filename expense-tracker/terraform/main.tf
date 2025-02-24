ba6b705166966d9b34b4d127c49941e09e97aa9e
AiZm8 <zai@ualberta.ca>
2025-02-24 01:23:50 -0700

    implement cloudfront

--- expense-tracker/terraform/main.tf
+++ expense-tracker/terraform/main.tf
@@ -48,55 +48,55 @@ resource "aws_s3_bucket_policy" "website" {
 
 # CloudFront distribution
 resource "aws_cloudfront_distribution" "website" {
-    enabled = true
-    is_ipv6_enabled = true 
-    default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled    = true
+  default_root_object = "index.html"
 
-    origin {
-        domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-        origin_id = aws_s3_bucket.website.id
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.website.id
 
-        s3_origin_config {
-            origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-        }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
-}
+  }
 
-default_cache_behaviour {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket.website.id
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.website.id
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
-        query_string = false 
-        cookies {
-            forward = "none"
-        }
+      query_string = false
+      cookies {
+        forward = "none"
+      }
     }
-}
+  }
 
-restrictions {
+  restrictions {
     geo_restriction {
-        restriction_type = "none"
+      restriction_type = "none"
     }
-}
+  }
 
-viewer_certificate {
+  viewer_certificate {
     cloudfront_default_certificate = true
-}
+  }
 
-# Handle React Router
-custom_error_response {
-    error_code = 403
-    response_code = 200
+  # Handle React Router
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
     response_page_path = "/index.html"
-}
+  }
 
-custom_error_response {
-    error_code = 404
-    response_code = 200
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
     response_page_path = "/index.html"
+  }
 }
 
 # Output both URLs
